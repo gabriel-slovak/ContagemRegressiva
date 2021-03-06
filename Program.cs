b34b0e39f13828fda5cf6f82dedd26accc22c29b
Gabriel Slovak <slovakgabriellopes@gmail.com>
2021-03-06 16:34:02 -0300

    finalização da atividade Contagem Regressiva

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ContagemRegressiva_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Contagem Regressiva");
+            Console.WriteLine("-------------------");
+            Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Pressione uma tecla para começar!");
+         Console.ResetColor();
+            Console.WriteLine();
+
+            Console.WriteLine("Preparar...");
+            Console.ReadKey();
+            Console.WriteLine("Apontar...");
+            Console.ReadKey();
+Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("FOGO!");
+            Console.WriteLine();
+        Console.ResetColor();
+        }
+    }
+}
