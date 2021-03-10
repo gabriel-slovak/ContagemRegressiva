007a8c7652d5abeb311eef01d49d348655cc6b3b
Diego Neri <diego@diegoneri.com>
2021-03-09 22:23:54 -0300

    Ajusta endentação
    Ajusta endentação do código

--- Program.cs
+++ Program.cs
@@ -1,29 +1,26 @@
-﻿using System;
+using System;
 
-namespace ContagemRegressiva_2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ContagemRegressiva_2 {
+    class Program {
+        static void Main(string[] args) {
             Console.WriteLine("-------------------");
             Console.WriteLine("Contagem Regressiva");
             Console.WriteLine("-------------------");
             Console.WriteLine();
 
-Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Pressione uma tecla para começar!");
-         Console.ResetColor();
+            Console.ResetColor();
             Console.WriteLine();
 
             Console.WriteLine("Preparar...");
             Console.ReadKey();
             Console.WriteLine("Apontar...");
             Console.ReadKey();
-Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("FOGO!");
             Console.WriteLine();
-        Console.ResetColor();
+            Console.ResetColor();
         }
     }
 }
